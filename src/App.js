342beeccd9f70e35653b8ca2b456fcb62bce19b9
JayJay-H <0911aza@naver.com>
2021-12-03 23:09:09 +0900

    #7.0 To Do List part One

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import { useState } from "react";
-import ToDoList from "./ToDoList";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [num, setNum] = useState(0);
-  function deleteToDo(id) {
-    const newTodos = todos.filter((todo) => id !== todo.id);
-    setTodos(newTodos);
-  }
-  function addTodo(title) {
-    setTodos([...todos, { title, id: num }]);
-    setNum((num) => num + 1);
-  }
-  return <ToDoList todos={todos} onDelete={deleteToDo} onAdd={addTodo} />;
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") return;
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  console.log(toDos);
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
