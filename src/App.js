abb40f112d0bd878d655ced9f3713fbe150f860e
JayJay-H <0911aza@naver.com>
2021-12-04 13:29:49 +0900

    Movie App part One

--- src/App.js
+++ src/App.js
@@ -2,85 +2,38 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [selected, setSelected] = useState({});
-  const [money, setMoney] = useState(0);
-  const [myCoin, setMyCoin] = useState(0);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setSelected({
-          price: json["0"].quotes.USD.price,
-          symbol: json["0"].symbol,
-        });
-        setLoading(false);
-      });
+    getMovies();
   }, []);
-  const onSelect = (event) => {
-    const selectedIndex = event.target.options.selectedIndex;
-    const selectedOption = event.target.options[selectedIndex];
-    const price = selectedOption.getAttribute("price");
-    const symbol = selectedOption.getAttribute("symbol");
-    setSelected({ price, symbol });
-    setMoney(0);
-    setMyCoin(0);
-  };
-  const onWriteMoney = (event) => {
-    setMoney(event.target.value);
-  };
-  const onWriteCoin = (event) => {
-    setMyCoin(event.target.value);
-  };
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
       {loading ? (
-        <strong>Loading...</strong>
+        <h1>Loading...</h1>
       ) : (
-        <>
-          <select onChange={onSelect} defaultValue={coins["0"].name}>
-            {coins.map((coin, i) => (
-              <option
-                value={i}
-                price={coin.quotes.USD.price}
-                symbol={coin.symbol}
-                key={coin.id}
-              >
-                {coin.name}
-              </option>
-            ))}
-          </select>
-          <hr />
-          <div>
-            <h2>How many money do you have?</h2>
-            <input
-              value={money}
-              onChange={onWriteMoney}
-              placeholder="Write here..."
-            />
-            {" USD"}
-            <div>
-              <span>
-                You can get {money / selected.price} {selected.symbol}
-              </span>
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <h2>{movie.title}</h2>
+              <img src={movie.medium_cover_image} alt="" />
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
             </div>
-          </div>
-          <hr />
-          <div>
-            <h2>How many coins do you have?</h2>
-            <input
-              value={myCoin}
-              onChange={onWriteCoin}
-              placeholder="Write here..."
-            />
-            {` ${selected.symbol}`}
-            <div>
-              <span>You can get {selected.price * myCoin} USD</span>
-            </div>
-          </div>
-        </>
+          ))}
+        </div>
       )}
     </div>
   );
